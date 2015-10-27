ba0f627661dd0f022607221b2df87ed2d01a5b35
Ripta Pasay <ripta@users.noreply.github.com>
2015-10-27 16:27:20 -0700

    main: tying it together

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+// import "fmt"
+import "os"
+
+import "github.com/codegangsta/cli"
+
+var genLength int
+var genShort bool
+var genQuiet bool
+
+var version = "0.1.0"
+
+func main() {
+	app := cli.NewApp()
+	
+	app.Name = "diceware"
+	app.Version = version
+
+	bannerBytes, _ := dataBannerBytes()
+	app.Usage = string(bannerBytes)
+
+	app.Action = GeneratePassphrase
+	app.Flags = []cli.Flag {
+		cli.IntFlag{
+			Name:  "n, num-words",
+			Value: 6,
+			Usage: "number of words in the passphrase",
+		},
+		cli.BoolFlag{
+			Name:  "q, quiet",
+			Usage: "when specified, only prints words, and not the generated numbers",
+		},
+		cli.BoolFlag{
+			Name:  "s, short",
+			Usage: "when specified, prints short form: no spaces between words",
+		},
+		cli.StringFlag{
+			Name:  "w, word-list",
+			Value: "diceware",
+			Usage: "name of word list to use (see `diceware list` output)",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "list",
+			Aliases: []string{"l", "li", "lis"},
+			Usage:   "list all available wordlists",
+			Action:  ListWordlistNames,
+		},
+	}
+
+	app.Run(os.Args)
+}
